3ceefe9ccbccb22e248d4e55ac59019ba2e25aa5
Eliel Sas <saseliel1@gmail.com>
2021-09-13 16:33:58 -0300

    script

---
+++ Script.js
@@ -0,0 +1,9 @@
+let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+for (let i = 1; i <= ingresarNumero; i++) {
+    let resultado = "ahora vas a este mensaje la cantidad de veces que elegiste :D "
+    alert(resultado);
+}
+
+
+
+
