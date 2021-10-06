f9d1adfcce85d9919dc17d9e11c75dc82a33dac4
Eliel Sas <saseliel1@gmail.com>
2021-10-06 08:46:44 -0300

    js

--- Script.js
+++ Script.js
@@ -1,9 +1,24 @@
-let ingresarNumero = parseInt(prompt("Ingresar Numero"));
-for (let i = 1; i <= ingresarNumero; i++) {
-    let resultado = "ahora vas a este mensaje la cantidad de veces que elegiste :D "
-    alert(resultado);
+let arrMenu = [
+   {menu: "Menu Gourmet", Precio:3400},
+   {menu: "Menu veggie", Precio: 2900},
+   {menu: "Menu con Carne", Precio: 3200},
+]
+const Price = document.getElementById('Price');
+Price1.innerHTML = arrMenu[2].Precio;
+Price2.innerHTML = arrMenu[0].Precio;
+Price3.innerHTML = arrMenu[1].Precio;
+const Tag = document.getElementById('tag');
+tag1.innerHTML = arrMenu[2].menu;
+tag2.innerHTML = arrMenu[0].menu;
+tag3.innerHTML = arrMenu[1].menu;
+
+let Value = prompt("Que menu desea ? ")
+let cantidad = prompt("Que cantidad ? ")
+if (Value = 1 ) {
+
+   alert("Su costo sera de $" + arrMenu[2].Precio*cantidad)
+} else if (Value = 2) {
+   alert("Su costo sera de $" + arrMenu[0].Precio*cantidad)
+} else if (Value = 3) {
+   alert("Su costo sera de $" + arrMenu[1].Precio*cantidad)
 }
-
-
-
-
