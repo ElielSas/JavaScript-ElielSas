50c0e9cd5a59a94ca830a82505c1c2e329a2ef3d
Eliel Sas <saseliel1@gmail.com>
2021-10-11 18:48:23 -0300

    remove

--- Script.js
+++ Script.js
@@ -27,8 +27,13 @@ arrMenu.forEach ( el => {
 
 const boton1 = document.getElementById("btn1");
 boton1.addEventListener("click", function(e) {
-	let dir = prompt("intruduzca su direccion: ");
-   localStorage.setItem(dir,arrMenu[0].menu)
+	let dir = prompt("intruduzca su direccion : ");
+   let tel = prompt("Introduzca su tel: ")
+   localStorage.setItem(dir+tel,arrMenu[0].menu)
+   if ( tel != [0-9]) {
+      alert("Solo se aceptan numeros");
+      localStorage.clear();
+   }
 });
 
 
@@ -36,18 +41,26 @@ boton1.addEventListener("click", function(e) {
 const boton2 = document.getElementById("btn2");
 boton2.addEventListener("click", function(e) {
 	let dir = prompt("intruduzca su direccion: ");
-   localStorage.setItem(dir,arrMenu[1].menu)
+   localStorage.setItem(dir,arrMenu[1].menu);
+   if ( tel != [0-9]) {
+      alert("Solo se aceptan numeros");
+      localStorage.clear();
+   }
 });
 
 const boton3 = document.getElementById("btn3");
 boton3.addEventListener("click", function(e) {
 	let dir = prompt("intruduzca su direccion: ");
-   localStorage.setItem(dir,arrMenu[2].menu)
+   localStorage.setItem(dir,arrMenu[2].menu);
+   if ( tel != [0-9]) {
+      alert("Solo se aceptan numeros");
+      localStorage.clear();
+   }
 });
 
 
-const carro = document.querySelector("#carro")
-if (boton1 == true) {
-   carro.innerHTML
-   `<p><a>${arrMenu[0].menu}</a><button id="remove">Remove</button></p>`
-}
+////const carro = document.querySelector("#carro")
+///if (boton1 == true) {
+  /// carr
+   ///`<p><a>${arrMenu[0].menu}</a><button id="remove">Remove</button></p>`
+///}
